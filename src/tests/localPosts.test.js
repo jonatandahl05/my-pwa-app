c8f557e5163887a6e9e19c55005818140fae5f38
kostas900402 <nascoskostas@gmail.com>
2026-02-17 09:08:35 +0100

    ett test

---
+++ src/tests/localPosts.test.js
@@ -0,0 +1,20 @@
+// src/tests/localPosts.test.js
+
+// @vitest-environment jsdom
+
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import {
+    loadLocalPosts
+} from '../storage/localPosts.js';
+
+beforeEach(() => {
+    localStorage.clear();
+})
+
+describe('loadLocalPosts', () => {
+
+    it('ska returnera tom array om localStorage är tomt', () => {
+        const result = loadLocalPosts();
+        expect(result).toEqual([]);
+    });
+});
